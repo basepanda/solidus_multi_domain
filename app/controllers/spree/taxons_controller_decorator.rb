7d6aea6cb890a2d638b9c1b89ec278c5a1ada64b
alexander mcdaid <alexander.mcdaid@gmail.com>
2017-02-02 00:52:19 +0000

    r

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::TaxonsController.class_eval do
-   def show
+  def show
     @taxon = Spree::Taxon.find_by_store_id_and_permalink!(current_store.id, params[:id])
     return unless @taxon
 
-    @searcher = build_searcher(params.merge(:taxon => @taxon.id))
+    @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
     @products = @searcher.retrieve_products
-    @taxonomies = get_taxonomies
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
 end if SpreeMultiDomain::Engine.frontend_available?
